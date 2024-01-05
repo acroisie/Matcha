9850f3bd25c73d913d386f32ae114f8df8b9ef8b
Arnaud <acroisie@student.42lyon.fr>
2024-01-05 16:17:15 +0100

    get almost working stuff

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_mongoengine import MongoEngine
+from flask_pymongo import PyMongo
 from dotenv import load_dotenv
 import os
 
@@ -7,14 +7,10 @@ load_dotenv()
 
 app = Flask(__name__)
 
-#MongoDB configuration
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['MONGODB_SETTINGS'] = {
-    'db': os.getenv('MONGODB_DB'),
-    'host': os.getenv('MONGODB_HOST')
-}
+app.config['MONGO_URI'] = os.getenv('MONGODB_HOST')
 
-db = MongoEngine(app)
+mongo = PyMongo(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
