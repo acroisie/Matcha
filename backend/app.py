7f72eb62a192d0c5ff7a329107598bc0b7bee9d4
Arnaud <acroisie@student.42lyon.fr>
2024-01-10 14:43:45 +0100

    Fix docker issue

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,36 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 from dotenv import load_dotenv
+from models.user import User
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['MONGO_URI'] = os.getenv('MONGODB_HOST')
 
 mongo = PyMongo(app)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    user = User(
+        username=data['username'],
+        email=data['email'],
+        password=data['password'],
+        first_name=data['first_name'],
+        last_name=data['last_name'],
+        gender=data['gender'],
+        sexual_preferences=data['sexual_preferences'],
+        bio=data['bio'],
+        interests=data['interests'],
+        profile_pictures=data['profile_pictures'],
+        fame_rating=data['fame_rating'],
+        location=data['location']
+    )
+    mongo.db.users.insert_one(user.to_json())
+    return jsonify({"message": "Registration succeeded"}), 201
--- backend/models/user.py
+++ backend/models/user.py
@@ -2,18 +2,18 @@ from werkzeug.security import generate_password_hash
 
 class User:
 	def __init__(self, username, email, password, first_name, last_name, gender, sexual_preferences, bio, interests, profile_pictures, fame_rating, location):
-        self.username = username
-        self.email = email
-        self.password = generate_password_hash(password)
-        self.first_name = first_name
-        self.last_name = last_name
-        self.gender = gender
-        self.sexual_preferences = sexual_preferences
-        self.bio = bio
-        self.interests = interests
-        self.profile_pictures = profile_pictures
-        self.fame_rating = fame_rating
-        self.location = location
+		self.username = username
+		self.email = email
+		self.password = generate_password_hash(password)
+		self.first_name = first_name
+		self.last_name = last_name
+		self.gender = gender
+		self.sexual_preferences = sexual_preferences
+		self.bio = bio
+		self.interests = interests
+		self.profile_pictures = profile_pictures
+		self.fame_rating = fame_rating
+		self.location = location
 
 	def to_json(self):
 		return {
